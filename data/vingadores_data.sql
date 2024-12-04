6f842762733b1c2f28d62576d9aa96024af75e26
luolibit <horadalu1@gmail.com>
2024-12-04 13:51:16 -0300

    Update vingadores_data.sql

--- data/vingadores_data.sql
+++ data/vingadores_data.sql
@@ -1,6 +1,8 @@
+CREATE DATABASE  IF NOT EXISTS `vingadores` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `vingadores`;
 -- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
 --
--- Host: localhost    Database: trabalho
+-- Host: localhost    Database: vingadores
 -- ------------------------------------------------------
 -- Server version	8.0.35
 
@@ -16,312 +18,253 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `autor`
+-- Table structure for table `chip_gps`
 --
 
-DROP TABLE IF EXISTS `autor`;
+DROP TABLE IF EXISTS `chip_gps`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `autor` (
-  `id_autor` int NOT NULL AUTO_INCREMENT,
-  `nome_autor` varchar(255) DEFAULT NULL,
-  `nacionalidade` varchar(45) DEFAULT NULL,
-  `data_nascimento` date NOT NULL,
-  PRIMARY KEY (`id_autor`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE `chip_gps` (
+  `id_chip_gps` int NOT NULL AUTO_INCREMENT,
+  `localização_atual` varchar(255) NOT NULL,
+  `ultima_localização` varchar(255) NOT NULL,
+  `id_tornozeleira` int NOT NULL,
+  PRIMARY KEY (`id_chip_gps`),
+  KEY `fk_id_tornozeleira_idx` (`id_tornozeleira`),
+  CONSTRAINT `fk_id_tornozeleira` FOREIGN KEY (`id_tornozeleira`) REFERENCES `tornozeleira` (`id_tornozeleira`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `autor`
+-- Dumping data for table `chip_gps`
 --
 
-LOCK TABLES `autor` WRITE;
-/*!40000 ALTER TABLE `autor` DISABLE KEYS */;
-INSERT INTO `autor` VALUES (1,'Monteiro Lobato','Brasileiro','1882-04-18'),(2,'Machado de Assis','Brasileiro','1908-09-29'),(3,'Clarice Lispector','Brasileiro','1920-12-10'),(4,'Guimarães rosa','Brasileiro','1908-06-27'),(5,'Jorge Amado','Brasileiro','1912-08-10');
-/*!40000 ALTER TABLE `autor` ENABLE KEYS */;
+LOCK TABLES `chip_gps` WRITE;
+/*!40000 ALTER TABLE `chip_gps` DISABLE KEYS */;
+/*!40000 ALTER TABLE `chip_gps` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `autor_genero`
+-- Table structure for table `convocacao`
 --
 
-DROP TABLE IF EXISTS `autor_genero`;
+DROP TABLE IF EXISTS `convocacao`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `autor_genero` (
-  `id_autor_genero` int NOT NULL AUTO_INCREMENT,
-  `id_autor` int NOT NULL,
-  `id_genero` int NOT NULL,
-  PRIMARY KEY (`id_autor_genero`),
-  KEY `id_autor` (`id_autor`),
-  KEY `id_genero` (`id_genero`),
-  CONSTRAINT `autor_genero_ibfk_1` FOREIGN KEY (`id_autor`) REFERENCES `autor` (`id_autor`),
-  CONSTRAINT `autor_genero_ibfk_2` FOREIGN KEY (`id_genero`) REFERENCES `genero` (`id_genero`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE `convocacao` (
+  `id_convocacao` int NOT NULL AUTO_INCREMENT,
+  `motivo` text NOT NULL,
+  `data_convocacao` datetime NOT NULL,
+  `data_comparecimento` datetime DEFAULT NULL,
+  `status` enum('Pendente','Comparecido','Ausente') NOT NULL,
+  `id_heroi` int NOT NULL,
+  PRIMARY KEY (`id_convocacao`),
+  KEY `fk_id_heroi_idx` (`id_heroi`),
+  CONSTRAINT `fk_id_heroi` FOREIGN KEY (`id_heroi`) REFERENCES `heroi` (`id_heroi`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `autor_genero`
+-- Dumping data for table `convocacao`
 --
 
-LOCK TABLES `autor_genero` WRITE;
-/*!40000 ALTER TABLE `autor_genero` DISABLE KEYS */;
-INSERT INTO `autor_genero` VALUES (1,1,1),(2,2,6),(3,3,7),(4,3,1),(5,4,6),(6,5,6);
-/*!40000 ALTER TABLE `autor_genero` ENABLE KEYS */;
+LOCK TABLES `convocacao` WRITE;
+/*!40000 ALTER TABLE `convocacao` DISABLE KEYS */;
+/*!40000 ALTER TABLE `convocacao` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `editora`
+-- Table structure for table `fraquezas`
 --
 
-DROP TABLE IF EXISTS `editora`;
+DROP TABLE IF EXISTS `fraquezas`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `editora` (
-  `id_editora` int NOT NULL AUTO_INCREMENT,
-  `nome_editora` varchar(255) DEFAULT NULL,
-  `endereco_editora` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id_editora`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE `fraquezas` (
+  `id_fraquezas` int NOT NULL AUTO_INCREMENT,
+  `nome_fraquezas` varchar(100) NOT NULL,
+  PRIMARY KEY (`id_fraquezas`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `editora`
+-- Dumping data for table `fraquezas`
 --
 
-LOCK TABLES `editora` WRITE;
-/*!40000 ALTER TABLE `editora` DISABLE KEYS */;
-INSERT INTO `editora` VALUES (1,'Saraiva','av. Sapopemba, 8134'),(2,'Abril','Praça Carlos Gomes, 42'),(3,'Globo','R. sete de abril, 261'),(4,'Vida','R. Conde de Sardezas, 246'),(5,'Record','R. do Paraiso, 139');
-/*!40000 ALTER TABLE `editora` ENABLE KEYS */;
+LOCK TABLES `fraquezas` WRITE;
+/*!40000 ALTER TABLE `fraquezas` DISABLE KEYS */;
+/*!40000 ALTER TABLE `fraquezas` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `editora_genero`
+-- Table structure for table `habilidade`
 --
 
-DROP TABLE IF EXISTS `editora_genero`;
+DROP TABLE IF EXISTS `habilidade`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `editora_genero` (
-  `id_editora_genero` int NOT NULL AUTO_INCREMENT,
-  `id_editora` int NOT NULL,
-  `id_genero` int NOT NULL,
-  PRIMARY KEY (`id_editora_genero`),
-  KEY `id_editora` (`id_editora`),
-  KEY `id_genero` (`id_genero`),
-  CONSTRAINT `editora_genero_ibfk_1` FOREIGN KEY (`id_editora`) REFERENCES `editora` (`id_editora`),
-  CONSTRAINT `editora_genero_ibfk_2` FOREIGN KEY (`id_genero`) REFERENCES `genero` (`id_genero`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE `habilidade` (
+  `id_habilidade` int NOT NULL AUTO_INCREMENT,
+  `nome_habilidade` varchar(100) NOT NULL,
+  PRIMARY KEY (`id_habilidade`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `editora_genero`
+-- Dumping data for table `habilidade`
 --
 
-LOCK TABLES `editora_genero` WRITE;
-/*!40000 ALTER TABLE `editora_genero` DISABLE KEYS */;
-INSERT INTO `editora_genero` VALUES (1,1,1),(2,1,6),(3,2,6),(4,3,6),(5,3,7),(6,4,1),(7,4,3),(8,5,1),(9,5,6);
-/*!40000 ALTER TABLE `editora_genero` ENABLE KEYS */;
+LOCK TABLES `habilidade` WRITE;
+/*!40000 ALTER TABLE `habilidade` DISABLE KEYS */;
+/*!40000 ALTER TABLE `habilidade` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `emprestimo`
+-- Table structure for table `heroi`
 --
 
-DROP TABLE IF EXISTS `emprestimo`;
+DROP TABLE IF EXISTS `heroi`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `emprestimo` (
-  `id_emprestimo` int NOT NULL AUTO_INCREMENT,
-  `data_emprestimo` date NOT NULL,
-  `id_livro` int NOT NULL,
-  `id_leitor` int NOT NULL,
-  PRIMARY KEY (`id_emprestimo`),
-  KEY `id_livro` (`id_livro`),
-  KEY `id_leitor` (`id_leitor`),
-  CONSTRAINT `emprestimo_ibfk_1` FOREIGN KEY (`id_livro`) REFERENCES `livro` (`id_livro`),
-  CONSTRAINT `emprestimo_ibfk_2` FOREIGN KEY (`id_leitor`) REFERENCES `leitor` (`id_leitor`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE `heroi` (
+  `id_heroi` int NOT NULL AUTO_INCREMENT,
+  `nome_heroi` varchar(45) DEFAULT NULL,
+  `nome_real` varchar(45) DEFAULT NULL,
+  `categoria` varchar(45) DEFAULT NULL,
+  `poderes` varchar(45) DEFAULT NULL,
+  `poder_principal` varchar(45) DEFAULT NULL,
+  `fraquezas` varchar(45) DEFAULT NULL,
+  `nivel_forca` int DEFAULT NULL,
+  PRIMARY KEY (`id_heroi`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `emprestimo`
+-- Dumping data for table `heroi`
 --
 
-LOCK TABLES `emprestimo` WRITE;
-/*!40000 ALTER TABLE `emprestimo` DISABLE KEYS */;
-INSERT INTO `emprestimo` VALUES (1,'2024-06-21',5,1),(2,'2024-01-27',4,2),(3,'2022-05-22',3,3),(4,'2024-08-15',2,4),(5,'2024-04-22',1,5),(6,'2023-09-21',3,5);
-/*!40000 ALTER TABLE `emprestimo` ENABLE KEYS */;
+LOCK TABLES `heroi` WRITE;
+/*!40000 ALTER TABLE `heroi` DISABLE KEYS */;
+INSERT INTO `heroi` VALUES (1,'Homem de Ferro','Tony Stark','Humano','Soltar raio,voar','Soltar raio','Impostos',10000),(2,'Homem Aranha','Peter Parker','Meta-Humano','Soltar teia,Aderência','Sotar teia','Responsabilidade,Emoções',8000),(3,'Hulk','Bruce Banner','Meta-Humano','Força','Força','Inteligência',10000);
+/*!40000 ALTER TABLE `heroi` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Temporary view structure for view `emprestimo_view`
+-- Table structure for table `heroi_fraquezas`
 --
 
-DROP TABLE IF EXISTS `emprestimo_view`;
-/*!50001 DROP VIEW IF EXISTS `emprestimo_view`*/;
-SET @saved_cs_client     = @@character_set_client;
-/*!50503 SET character_set_client = utf8mb4 */;
-/*!50001 CREATE VIEW `emprestimo_view` AS SELECT 
- 1 AS `id_emprestimo`,
- 1 AS `nome_leitor`,
- 1 AS `nome_livro`,
- 1 AS `data_emprestimo`*/;
-SET character_set_client = @saved_cs_client;
-
---
--- Table structure for table `genero`
---
-
-DROP TABLE IF EXISTS `genero`;
+DROP TABLE IF EXISTS `heroi_fraquezas`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `genero` (
-  `id_genero` int NOT NULL AUTO_INCREMENT,
-  `nome_genero` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id_genero`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE `heroi_fraquezas` (
+  `id_heroi_fraquezas` int NOT NULL AUTO_INCREMENT,
+  `id_heroi` int NOT NULL,
+  `id_fraquezas` int NOT NULL,
+  PRIMARY KEY (`id_heroi_fraquezas`),
+  KEY `id_fraquezas_idx` (`id_fraquezas`),
+  KEY `fk_id_heroi_her_fra_idx` (`id_heroi`),
+  CONSTRAINT `fk_id_fraquezas_her_fra` FOREIGN KEY (`id_fraquezas`) REFERENCES `fraquezas` (`id_fraquezas`),
+  CONSTRAINT `fk_id_heroi_her_fra` FOREIGN KEY (`id_heroi`) REFERENCES `heroi` (`id_heroi`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `genero`
+-- Dumping data for table `heroi_fraquezas`
 --
 
-LOCK TABLES `genero` WRITE;
-/*!40000 ALTER TABLE `genero` DISABLE KEYS */;
-INSERT INTO `genero` VALUES (1,'Drama'),(2,'Suspense'),(3,'Ação'),(4,'Terror'),(5,'Comédia'),(6,'realismo'),(7,'intimismo');
-/*!40000 ALTER TABLE `genero` ENABLE KEYS */;
+LOCK TABLES `heroi_fraquezas` WRITE;
+/*!40000 ALTER TABLE `heroi_fraquezas` DISABLE KEYS */;
+/*!40000 ALTER TABLE `heroi_fraquezas` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `leitor`
+-- Table structure for table `heroi_habilidade`
 --
 
-DROP TABLE IF EXISTS `leitor`;
+DROP TABLE IF EXISTS `heroi_habilidade`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `leitor` (
-  `id_leitor` int NOT NULL AUTO_INCREMENT,
-  `nome_leitor` varchar(255) DEFAULT NULL,
-  `cpf_leitor` bigint NOT NULL,
-  `endereco_leitor` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id_leitor`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE `heroi_habilidade` (
+  `id_heroi_habilidade` int NOT NULL AUTO_INCREMENT,
+  `id_heroi` int NOT NULL,
+  `id_habilidade` int NOT NULL,
+  PRIMARY KEY (`id_heroi_habilidade`),
+  CONSTRAINT `fk_id_habilidade_her_hab` FOREIGN KEY (`id_heroi_habilidade`) REFERENCES `habilidade` (`id_habilidade`),
+  CONSTRAINT `fk_id_heroi_her_hab` FOREIGN KEY (`id_heroi_habilidade`) REFERENCES `heroi` (`id_heroi`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `leitor`
+-- Dumping data for table `heroi_habilidade`
 --
 
-LOCK TABLES `leitor` WRITE;
-/*!40000 ALTER TABLE `leitor` DISABLE KEYS */;
-INSERT INTO `leitor` VALUES (1,'Matheus Inouye',1111111111,'av. nazaré 402'),(2,'Bruno Dutra',1111111112,'av. barão de ladario 24'),(3,'Felipe Dorneles',1111111222,'av. carrão 71'),(4,'Guilherme Kenzo',1111111232,'av. sonata aurora 420'),(5,'Gustavo Amorim',1111111233,'av. the goat  01');
-/*!40000 ALTER TABLE `leitor` ENABLE KEYS */;
+LOCK TABLES `heroi_habilidade` WRITE;
+/*!40000 ALTER TABLE `heroi_habilidade` DISABLE KEYS */;
+/*!40000 ALTER TABLE `heroi_habilidade` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `livro`
+-- Table structure for table `mandado_prisao`
 --
 
-DROP TABLE IF EXISTS `livro`;
+DROP TABLE IF EXISTS `mandado_prisao`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `livro` (
-  `id_livro` int NOT NULL AUTO_INCREMENT,
-  `nome_livro` varchar(255) DEFAULT NULL,
-  `data_publicacao` date NOT NULL,
-  PRIMARY KEY (`id_livro`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE `mandado_prisao` (
+  `id_mandado_prisao` int NOT NULL AUTO_INCREMENT,
+  `motivo` text NOT NULL,
+  `data_emissao` datetime NOT NULL,
+  `status` enum('Pendente','Cumprido','Cancelado') NOT NULL,
+  `id_heroi` int NOT NULL,
+  PRIMARY KEY (`id_mandado_prisao`),
+  KEY `fk_heroi_id_idx` (`id_heroi`),
+  CONSTRAINT `fk_heroi_id` FOREIGN KEY (`id_heroi`) REFERENCES `heroi` (`id_heroi`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `livro`
+-- Dumping data for table `mandado_prisao`
 --
 
-LOCK TABLES `livro` WRITE;
-/*!40000 ALTER TABLE `livro` DISABLE KEYS */;
-INSERT INTO `livro` VALUES (1,'Dom Casmurro','1899-09-21'),(2,'Tieta','1970-08-17'),(3,'O Saci','1921-05-30'),(4,'Covert Joy','1971-08-20'),(5,'Sagaran','1946-03-18');
-/*!40000 ALTER TABLE `livro` ENABLE KEYS */;
+LOCK TABLES `mandado_prisao` WRITE;
+/*!40000 ALTER TABLE `mandado_prisao` DISABLE KEYS */;
+/*!40000 ALTER TABLE `mandado_prisao` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `livro_autor`
+-- Table structure for table `tornozeleira`
 --
 
-DROP TABLE IF EXISTS `livro_autor`;
+DROP TABLE IF EXISTS `tornozeleira`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `livro_autor` (
-  `id_livro_autor` int NOT NULL AUTO_INCREMENT,
-  `id_livro` int NOT NULL,
-  `id_autor` int NOT NULL,
-  PRIMARY KEY (`id_livro_autor`),
-  KEY `id_livro` (`id_livro`),
-  KEY `id_autor` (`id_autor`),
-  CONSTRAINT `livro_autor_ibfk_1` FOREIGN KEY (`id_livro`) REFERENCES `livro` (`id_livro`),
-  CONSTRAINT `livro_autor_ibfk_2` FOREIGN KEY (`id_autor`) REFERENCES `autor` (`id_autor`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE `tornozeleira` (
+  `id_tornozeleira` int NOT NULL AUTO_INCREMENT,
+  `status` enum('Ativo','Inativo') NOT NULL,
+  `data_ativacao` datetime DEFAULT NULL,
+  `data_desativação` datetime DEFAULT NULL,
+  `id_heroi` int NOT NULL,
+  PRIMARY KEY (`id_tornozeleira`),
+  KEY `fk_id_heroi_idx` (`id_heroi`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `livro_autor`
+-- Dumping data for table `tornozeleira`
 --
 
-LOCK TABLES `livro_autor` WRITE;
-/*!40000 ALTER TABLE `livro_autor` DISABLE KEYS */;
-INSERT INTO `livro_autor` VALUES (1,4,3),(2,1,2),(3,5,4),(4,3,1),(5,2,5);
-/*!40000 ALTER TABLE `livro_autor` ENABLE KEYS */;
+LOCK TABLES `tornozeleira` WRITE;
+/*!40000 ALTER TABLE `tornozeleira` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tornozeleira` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `publicacao`
+-- Dumping events for database 'vingadores'
 --
 
-DROP TABLE IF EXISTS `publicacao`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `publicacao` (
-  `id_publicacao` int NOT NULL AUTO_INCREMENT,
-  `id_livro` int NOT NULL,
-  `id_editora` int NOT NULL,
-  `id_autor` int NOT NULL,
-  PRIMARY KEY (`id_publicacao`),
-  KEY `id_livro` (`id_livro`),
-  KEY `id_editora` (`id_editora`),
-  KEY `id_autor` (`id_autor`),
-  CONSTRAINT `publicacao_ibfk_1` FOREIGN KEY (`id_livro`) REFERENCES `livro` (`id_livro`),
-  CONSTRAINT `publicacao_ibfk_2` FOREIGN KEY (`id_editora`) REFERENCES `editora` (`id_editora`),
-  CONSTRAINT `publicacao_ibfk_3` FOREIGN KEY (`id_autor`) REFERENCES `autor` (`id_autor`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
 --
--- Dumping data for table `publicacao`
+-- Dumping routines for database 'vingadores'
 --
-
-LOCK TABLES `publicacao` WRITE;
-/*!40000 ALTER TABLE `publicacao` DISABLE KEYS */;
-INSERT INTO `publicacao` VALUES (1,1,4,2),(2,2,5,5),(3,3,3,1),(4,4,2,3),(5,5,1,4);
-/*!40000 ALTER TABLE `publicacao` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Final view structure for view `emprestimo_view`
---
-
-/*!50001 DROP VIEW IF EXISTS `emprestimo_view`*/;
-/*!50001 SET @saved_cs_client          = @@character_set_client */;
-/*!50001 SET @saved_cs_results         = @@character_set_results */;
-/*!50001 SET @saved_col_connection     = @@collation_connection */;
-/*!50001 SET character_set_client      = cp850 */;
-/*!50001 SET character_set_results     = cp850 */;
-/*!50001 SET collation_connection      = cp850_general_ci */;
-/*!50001 CREATE ALGORITHM=UNDEFINED */
-/*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
-/*!50001 VIEW `emprestimo_view` AS select `emprestimo`.`id_emprestimo` AS `id_emprestimo`,`leitor`.`nome_leitor` AS `nome_leitor`,`livro`.`nome_livro` AS `nome_livro`,`emprestimo`.`data_emprestimo` AS `data_emprestimo` from ((`emprestimo` join `leitor` on((`emprestimo`.`id_leitor` = `leitor`.`id_leitor`))) join `livro` on((`emprestimo`.`id_livro` = `livro`.`id_livro`))) */;
-/*!50001 SET character_set_client      = @saved_cs_client */;
-/*!50001 SET character_set_results     = @saved_cs_results */;
-/*!50001 SET collation_connection      = @saved_col_connection */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -332,4 +275,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-11-27 16:40:12
+-- Dump completed on 2024-12-04 13:49:10
